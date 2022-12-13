5bde2be2501c7cf55cceb62ac5d3cac2202a154c
Narender Singh <97868320+narendersinghh@users.noreply.github.com>
2022-12-13 20:36:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let allowedSites = ['https://www.digiintern.com','https://www.nopaperforms.com','https://dheerajarora1997.github.io']
+let allowedSites = ['https://www.digiintern.com','https://www.nopaperforms.com','https://dheerajarora1997.github.io', 'https://omsaipacker.com']
 
 
 let domainName = document.location.origin;
